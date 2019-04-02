e961999c3dd3d5043b56af1ead0612fc70bb09ad
Greg Hendershott <greghendershott@gmail.com>
2019-04-02 10:04:03 -0400

    Format code to group/emphasize the "constants"

--- rkt/site.rkt
+++ rkt/site.rkt
@@ -4,20 +4,16 @@
          racket/format
          net/uri-codec)
 
-(provide scheme
+(provide author
+         scheme
          host
          full-uri
-         urn
-         author)
+         urn)
 
-(define (scheme)
-  "https")
-
-(define (host)
-  "www.greghendershott.com")
-
-(define (host/urn)
-  "urn:www-greghendershott-com")
+(define (author)   "Greg Hendershott")
+(define (scheme)   "https")
+(define (host)     "www.greghendershott.com")
+(define (host/urn) "urn:www-greghendershott-com")
 
 (define/contract (full-uri uri-path)
   (-> (and/c path-string? relative-path?) string?)
@@ -26,6 +22,3 @@
 (define (urn uri-path)
   ;; Note that URNs have a more restricted syntax than URIs.
   (~a (host/urn) ":" (regexp-replace* #px"/" uri-path "-")))
-
-(define (author)
-  "Greg Hendershott")
